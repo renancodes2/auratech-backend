76bfe2cf521fb57260bfa49c83644fa209bd21c0
renelps <renanlps616@gmail.com>
2025-12-09 11:16:29 -0300

    feat: Configure Prisma ORM and setup initial database connection

---
+++ prisma/migrations/20251208211228_new_model_product_and_category/migration.sql
@@ -0,0 +1,54 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Cart` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `CartItem` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Order` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `OrderItem` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Review` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Cart" DROP CONSTRAINT "Cart_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "CartItem" DROP CONSTRAINT "CartItem_cartId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "CartItem" DROP CONSTRAINT "CartItem_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Order" DROP CONSTRAINT "Order_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderItem" DROP CONSTRAINT "OrderItem_orderId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderItem" DROP CONSTRAINT "OrderItem_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Review" DROP CONSTRAINT "Review_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Review" DROP CONSTRAINT "Review_userId_fkey";
+
+-- DropTable
+DROP TABLE "Cart";
+
+-- DropTable
+DROP TABLE "CartItem";
+
+-- DropTable
+DROP TABLE "Order";
+
+-- DropTable
+DROP TABLE "OrderItem";
+
+-- DropTable
+DROP TABLE "Review";
+
+-- DropEnum
+DROP TYPE "OrderStatus";
+
+-- DropEnum
+DROP TYPE "UserRole";
