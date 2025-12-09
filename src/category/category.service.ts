db177a6ce7be69e0cd1358f267bcf395b919b6b0
renelps <renanlps616@gmail.com>
2025-12-09 11:21:24 -0300

    feat: Implement CategoryModule (controller, service, and DTOs)

---
+++ src/category/category.service.ts
@@ -0,0 +1,26 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { createCategoryDto } from './dto/create-category.dto';
+
+@Injectable()
+export class CategoryService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createCategory(data: createCategoryDto) {
+    try {
+      const create = await this.prisma.category.create({
+        data: {
+          name: data.name,
+          description: data.description,
+        },
+      });
+
+      return create;
+    } catch (err) {
+      throw new HttpException(
+        'Failed to create category',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+}
