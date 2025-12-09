28291848adbad5035ce8ee3d7538950326439e05
renelps <renanlps616@gmail.com>
2025-12-09 11:17:59 -0300

    feat(bootstrap): Configure global prefix and start server on port 3333

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT ?? 3333);
+}
+bootstrap();
